829b0943b5b92dcdf9b45ff3a7727412d4fe0864
samuel <parisasamuel@gmail.com>
2026-09-21 19:06:40 -0400

    feat: add movie model and sample data

---
+++ lib/data/movies_data.dart
@@ -0,0 +1,63 @@
+import '../models/movie.dart';
+
+final List<Movie> sampleMovies = [
+  Movie(
+    title: 'Inception',
+    posterPath: 'assets/images/inception.jpg',
+    cast: [
+      'Leonardo DiCaprio',
+      'Joseph Gordon-Levitt',
+      'Elliot Page',
+    ],
+    synopsis:
+        'A skilled thief enters the dreams of others to steal secrets and is given a difficult mission involving inception.',
+  ),
+
+  Movie(
+    title: 'The Matrix',
+    posterPath: 'assets/images/matrix.jpg',
+    cast: [
+      'Keanu Reeves',
+      'Laurence Fishburne',
+      'Carrie-Anne Moss',
+    ],
+    synopsis:
+        'A computer programmer discovers that the world he knows is actually a simulated reality.',
+  ),
+
+  Movie(
+    title: 'Interstellar',
+    posterPath: 'assets/images/interstellar.jpg',
+    cast: [
+      'Matthew McConaughey',
+      'Anne Hathaway',
+      'Jessica Chastain',
+    ],
+    synopsis:
+        'A group of explorers travel through space to find a new home for humanity.',
+  ),
+
+  Movie(
+    title: 'Avengers: Endgame',
+    posterPath: 'assets/images/endgame.jpg',
+    cast: [
+      'Robert Downey Jr.',
+      'Chris Evans',
+      'Scarlett Johansson',
+    ],
+    synopsis:
+        'The remaining Avengers attempt to reverse the devastating events that changed the universe.',
+  ),
+
+  Movie(
+    title: 'Spider-Man: No Way Home',
+    posterPath: 'assets/images/spiderman.jpg',
+    cast: [
+      'Tom Holland',
+      'Zendaya',
+      'Benedict Cumberbatch',
+    ],
+    synopsis:
+        'Peter Parker asks for help after his identity is revealed, leading to unexpected consequences.',
+  ),
+];
